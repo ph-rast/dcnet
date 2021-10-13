8844c0ee4d4c8686a67d8753f58155353b520aaf
Philippe Rast <rast.ph@gmail.com>
2021-10-13 13:50:51 -0700

    Minor - Plotting individual correlations

--- tests/simulated.R
+++ tests/simulated.R
@@ -49,7 +49,7 @@ stanmodel
 model_fit <- rstan::sampling(stanmodel,
                                      data = stan_data,
                                      verbose = TRUE,
-                                     iter = 10,
+                                     iter = 100,
                                      #control = list(adapt_delta = .99),
                                      chains = 1,
                                      init_r = .05)
@@ -58,7 +58,64 @@ model_fit <- rstan::sampling(stanmodel,
 options(width = 220 )
 #model_fit
 
-print(model_fit, pars =  c("H", 'UPs')) ## What's up wit the stdnorms? Seem WAY to large
+print(model_fit, pars =  c("H")) ## What's up wit the stdnorms? Seem WAY to large
+
+tail( rstan::summary(model_fit, pars = "H")$summary , n =  18)
+N
+H <- array( rstan::summary(model_fit, pars = "H")$summary[,1], dim = c(N, tl, 3, 3 ) )
+H <- array( rstan::summary(model_fit, pars = "H")$summary[,"mean"], dim = c(3, 3, N, tl) )
+
+r1 <- array(NA, dim = c(N, tl ) )
+r2 <- r1
+r3 <- r1
+
+for( i in 1:N ) {
+    for(k in 1:tl ) {
+        r1[i,k] <- cov2cor(H[,,i,k])[2,1]
+        r2[i,k] <- cov2cor(H[,,i,k])[3,1]
+        r3[i,k] <- cov2cor(H[,,i,k])[3,2]
+    }
+}
+
+plot(1:tl, r1[1,], type =  'l', ylim = c(-1, 1 ), col = "#3299ff60")
+for(i in 2:N ) {
+    lines(r1[i,],  col = "#3299ff60" )
+}
+
+
+op <- par(mfcol = c(2, 2 ) )
+plot(1:tl, r1[1,], type =  'l', ylim = c(-1, 1 ), col = 2)
+for(i in 2:N ) {
+    lines(r1[i,],  col = i)#paste0("#99f", sprintf("%02d", 1), "f60") )
+}
+
+plot(1:tl, r2[1,], type =  'l', ylim = c(-1, 1 ), col = 2)
+for(i in 2:N ) {
+    lines(r2[i,],  col = i)#paste0("#99f", sprintf("%02d", 1), "f60") )
+}
+
+plot(1:tl, r3[1,], type =  'l', ylim = c(-1, 1 ), col = 2)
+for(i in 2:N ) {
+    lines(r3[i,],  col = i)#paste0("#99f", sprintf("%02d", 1), "f60") )
+}
+
+op
+
+
+mnr1 <-NA# array(NA, dim = c(1,  tl ) )
+## Average over N's
+for( k in 1:tl ) {
+    mnr1[k] <- mean(H[1,2,,k]) /( sqrt( mean(H[1,1,,k]) * mean(H[2,2,,k]) ) )
+}
+mnr1
+
+plot(1:tl, r1[1,], type =  'l', ylim = c(-1, 1 ), col = "#5033ff60")
+for(i in 2:N ) {
+    lines(r1[i,],  col = "#5033ff60") 
+}
+lines(mnr1, col = 1)
+
+print(model_fit, pars =  c("S")) 
 
 
 
