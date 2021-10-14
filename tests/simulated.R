749323883c4f384a31cc04ea145089158c34f79a
Philippe Rast <rast.ph@gmail.com>
2021-10-14 11:34:15 -0700

    Ran on Nestler data - time to add ranef to R in DRD

--- tests/simulated.R
+++ tests/simulated.R
@@ -1,8 +1,8 @@
 library(dcnet )
 
 ## Create data:
-N <- 20
-tl <- 40
+N <- 5
+tl <- 10
 nts <- 3
 simdat <- dcnet:::.simuDCC(tslength = tl,  N = N,  n_ts = nts,  ranef_sd_S = 0.0001 )
 simdat[[1]]
@@ -13,6 +13,7 @@ X
 X <- array(0,  dim = c(N*tl, nts ) )
 
 groupvec <- rep(c(1:N),  each = tl )
+groupvec
 
 return_standat <- dcnet:::standat( data = X, J = N, group = groupvec, xC = groupvec,  P = 1,
                                   standardize_data = TRUE,
@@ -26,9 +27,12 @@ stan_data <- return_standat[ c("T", "xC", "rts", "nt",
 
 stan_data
 stan_data$rts
-stan_data$rts <- array( apply(simdat[[1]], 1, FUN = rbind ), dim = c(tl, N, nts) )
+stan_data$rts <-
+    array( apply(simdat[[1]], 1, FUN = rbind ), dim = c(tl, N, nts) )
 ## same as this from X:  array(X,  dim = c(20, 4, 3) )
 
+
+stan_data$rts[,,3]
 
 
 simdat[[1]][,,4]
@@ -62,44 +66,69 @@ print(model_fit, pars =  c("H")) ## What's up wit the stdnorms? Seem WAY to larg
 
 tail( rstan::summary(model_fit, pars = "H")$summary , n =  18)
 N
-H <- array( rstan::summary(model_fit, pars = "H")$summary[,1], dim = c(N, tl, 3, 3 ) )
-H <- array( rstan::summary(model_fit, pars = "H")$summary[,"mean"], dim = c(3, 3, N, tl) )
+                                        #H <- array( rstan::summary(model_fit, pars = "H")$summary[,1], dim = c(N, tl, 3, 3 ) )
+nts
+H <- array( rstan::summary(model_fit, pars = "H")$summary[,"mean"], dim = c(nts, nts, N, tl) )
 
-r1 <- array(NA, dim = c(N, tl ) )
-r2 <- r1
-r3 <- r1
+r21 <- array(NA, dim = c(N, tl ) )
+r31 <- r21
+r41 <- r21
+r32 <- r21
+r42 <- r21
+r43 <- r21
+
 
 for( i in 1:N ) {
     for(k in 1:tl ) {
-        r1[i,k] <- cov2cor(H[,,i,k])[2,1]
-        r2[i,k] <- cov2cor(H[,,i,k])[3,1]
-        r3[i,k] <- cov2cor(H[,,i,k])[3,2]
+        r21[i,k] <- cov2cor(H[,,i,k])[2,1]
+        r31[i,k] <- cov2cor(H[,,i,k])[3,1]
+        r41[i,k] <- cov2cor(H[,,i,k])[4,1]
+        r32[i,k] <- cov2cor(H[,,i,k])[3,2]
+        r42[i,k] <- cov2cor(H[,,i,k])[4,2]
+        r43[i,k] <- cov2cor(H[,,i,k])[4,3]
     }
 }
 
-plot(1:tl, r1[1,], type =  'l', ylim = c(-1, 1 ), col = "#3299ff60")
+
+#Sys.setenv("DISPLAY"=":0.0")
+
+pdf(file = "./local/corr.pdf", width = 10, height =  6)
+plot(1:tl, r21[1,], type =  'l', ylim = c(-1, 1 ), col = "#3299ff60")
 for(i in 2:N ) {
-    lines(r1[i,],  col = "#3299ff60" )
+    lines(r21[i,],  col = "#3299ff60" )
 }
+dev.off( )
 
 
-op <- par(mfcol = c(2, 2 ) )
-plot(1:tl, r1[1,], type =  'l', ylim = c(-1, 1 ), col = 2)
+pdf(file = "./local/corr.pdf", width = 10, height =  6)
+op <- par(mfcol = c(2, 3 ) )
+plot(1:tl, r21[1,], type =  'l', ylim = c(-1, 1 ), col = 2)
 for(i in 2:N ) {
-    lines(r1[i,],  col = i)#paste0("#99f", sprintf("%02d", 1), "f60") )
+    lines(r21[i,],  col = i)#paste0("#99f", sprintf("%02d", 1), "f60") )
 }
-
-plot(1:tl, r2[1,], type =  'l', ylim = c(-1, 1 ), col = 2)
+plot(1:tl, r31[1,], type =  'l', ylim = c(-1, 1 ), col = 2)
 for(i in 2:N ) {
-    lines(r2[i,],  col = i)#paste0("#99f", sprintf("%02d", 1), "f60") )
+    lines(r31[i,],  col = i)#paste0("#99f", sprintf("%02d", 1), "f60") )
 }
-
-plot(1:tl, r3[1,], type =  'l', ylim = c(-1, 1 ), col = 2)
+plot(1:tl, r41[1,], type =  'l', ylim = c(-1, 1 ), col = 2)
 for(i in 2:N ) {
-    lines(r3[i,],  col = i)#paste0("#99f", sprintf("%02d", 1), "f60") )
+    lines(r41[i,],  col = i)#paste0("#99f", sprintf("%02d", 1), "f60") )
 }
-
+plot(1:tl, r32[1,], type =  'l', ylim = c(-1, 1 ), col = 2)
+for(i in 2:N ) {
+    lines(r32[i,],  col = i)#paste0("#99f", sprintf("%02d", 1), "f60") )
+}
+plot(1:tl, r42[1,], type =  'l', ylim = c(-1, 1 ), col = 2)
+for(i in 2:N ) {
+    lines(r42[i,],  col = i)#paste0("#99f", sprintf("%02d", 1), "f60") )
+}
+plot(1:tl, r43[1,], type =  'l', ylim = c(-1, 1 ), col = 2)
+for(i in 2:N ) {
+    lines(r43[i,],  col = i)#paste0("#99f", sprintf("%02d", 1), "f60") )
+}
 op
+dev.off( )
+
 
 
 mnr1 <-NA# array(NA, dim = c(1,  tl ) )
@@ -122,3 +151,72 @@ print(model_fit, pars =  c("S"))
 fit <- dcnet( data = X, J = 4, group = groupvec, iterations = 500, standardize_data = TRUE)
 
 fit
+
+
+## Real data: Nestler
+dfs <- read.table("./local/dataFlip.txt", header = TRUE ) 
+head( dfs )
+
+dat1 <- dfs[,c("id","day", "gm_sociable",  "gm_creative", "gm_friendly", "gm_organised")]
+
+## make wide to create NA's and equal length for all
+X0 <- reshape(dat1, idvar = "id", timevar = "day", direction = "wide",
+        v.names = c("gm_sociable",  "gm_creative", "gm_friendly", "gm_organised"))
+
+## REplace missings with 0
+X0[is.na(X0 )] <- 0
+
+head(X0 )
+length(names(X0))
+
+XL <- reshape(X0, direction = "long",
+              idvar = "id", sep = ".",
+              varying = names(X0)[2:329] )
+
+## sort by id, then time
+X2 <- XL[order( XL$id,  XL$time), ]
+
+N <- max( X2$id )
+N
+groupvec <- X2$id
+groupvec
+tl <- dim(XL )[1]/ N
+tl
+
+head(XL )
+length(groupvec )
+nts <- 4 # Number of variables
+
+return_standat <- dcnet:::standat( data = X2[,c("gm_sociable",  "gm_creative", "gm_friendly", "gm_organised")],
+                                  J = N, group = groupvec, xC = groupvec,  P = 1,
+                                  standardize_data = TRUE,
+                                  Q = 1, distribution = 0, meanstructure = "VAR")
+
+
+stan_data <- return_standat[ c("T", "xC", "rts", "nt",
+                                   "distribution", "P", "Q",
+                                   "meanstructure", "J", "nobs", "group")]
+
+rts <- array(NA, dim = c(tl, N, nts))
+
+
+rts[,,1] <- array( X2[,c("gm_sociable")], dim = c(tl, N) ) ## time by person
+rts[,,2] <- array( X2[,c("gm_creative")], dim = c(tl, N) ) ## time by person
+rts[,,3] <- array( X2[,c("gm_friendly")], dim = c(tl, N) ) ## time by person
+rts[,,4] <- array( X2[,c("gm_organised")], dim = c(tl, N) ) ## time by person
+
+stan_data$rts <- rts
+
+
+
+model_fit <- rstan::sampling(stanmodel,
+                                     data = stan_data,
+                                     verbose = TRUE,
+                                     iter = 100,
+                                     #control = list(adapt_delta = .99),
+                                     chains = 4,
+                                     init_r = .05)
+
+
+options(width = 220 )
+#model_fit
