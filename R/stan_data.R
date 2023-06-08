cab6e1667a6f48b4a48d015e36cb0078e633f5a1
Philippe Rast <rast.ph@gmail.com>
2023-06-08 12:45:19 +0200

    Data centering and std now maintains differences across J. Grand mean centering instead of person-mean (same for SD)

--- R/stan_data.R
+++ R/stan_data.R
@@ -68,7 +68,7 @@ stan_data <- function(data, J, group, xC, S_pred, P = 1, Q = 1, standardize_data
   ## }
 
   ## TODO: Generalize to unequal numbers of observations per individual
-  ## WARNIGN: This assumes equal numbers of observations for everyone!
+  ## WARNING: This assumes equal numbers of observations for everyone!
   T <- nrow(data[[1]])
   nobs <- T * J
 
@@ -78,17 +78,23 @@ stan_data <- function(data, J, group, xC, S_pred, P = 1, Q = 1, standardize_data
     varT[i] <- nrow(data[[i]])
   }
   if( var(varT) > 0) stop("Time-series lengths are not equal for all groups/subjects")
-  
+
+  ## Standardization and Centering: Maintain relative locations of time series across individuals/groups to
+  ## obtain meaningful random intercept terms.
+  ## Same for centering only (when not standardizing). Relative position needs to be maintained.
+  ## Obtain Means and SD across J (grouping) within the same variable. 
   if( standardize_data ) {
-    ## Standardize time-series
+    ## Standardize time-series. 
     stdx <- data
-    centered_data <- matrix(NA, nrow = J, ncol = nt)
-    scaled_data <- matrix(NA, nrow = J, ncol = nt)
+
+    ##  Obtain column means across all J
+    col_means <- colMeans( do.call(rbind,  stdx) )
+    sd_means <- apply( do.call(rbind,  stdx), 2, sd )
     
+    ## First convert integers to numeric, then standardize    
     for( i in 1:J ) {
-      stdx[[i]] <- scale(data[[i]] )
-      centered_data[i,] <- attr(stdx[[i]], "scaled:center")
-      scaled_data[i,] <- attr(stdx[[i]], "scaled:scale")  
+      stdx[[i]] <- apply( data[[i]], MARGIN = 2, as.numeric ) ## make numeric
+      stdx[[i]] <- (stdx[[i]] - col_means) / sd_means  
     }
     
     return_standat <- list(T = T, ## TODO: may need to allow different T's per J
@@ -96,8 +102,8 @@ stan_data <- function(data, J, group, xC, S_pred, P = 1, Q = 1, standardize_data
                            xC = xC,
                            S_pred = S_pred,
                            nt = ncol(stdx[[1]]),
-                           centered_data = centered_data,
-                           scaled_data = scaled_data,
+                           centered_data = col_means, 
+                           scaled_data = sd_means, 
                            distribution = distribution,
                            P = P,
                            Q = Q,
@@ -111,19 +117,22 @@ stan_data <- function(data, J, group, xC, S_pred, P = 1, Q = 1, standardize_data
   } else {
     ## Unstandardized, only centered
     ctrx <- data
-    centered_data <- matrix(NA, nrow = J, ncol = nt)
-    
-    for( i in 1:J ) {
-      ctrx[[i]] <- scale(data[[i]], center = TRUE, scale = FALSE)
-      centered_data[i,] <- attr(ctrx[[i]], "scaled:center")
-    }
 
+    ##  Obtain column means across all J
+    col_means <- colMeans( do.call(rbind,  ctrx) )
+    
+    ## First convert integers to numeric, then subtract grand col_means to center
+     for( i in 1:J ) {
+       ctrx[[i]] <- apply( data[[i]], MARGIN = 2, as.numeric ) #scale(data[[i]], center = FALSE, scale = FALSE)
+       ctrx[[i]] <- ctrx[[i]] - col_means 
+     }
+    
     return_standat <- list(T = T, ## TODO: may need to allow different T's per J
                            rts = ctrx,
                            xC = xC,
                            S_pred = S_pred,
                            nt = ncol(ctrx[[1]]),
-                           centered_data = centered_data,
+                           centered_data = col_means,
                            distribution = distribution,
                            P = P,
                            Q = Q,
